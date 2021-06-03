ce85be960ceb51d03a7fd25c6d96ec80942a8041
olaifa olawale <olaifaolawaleh@gmail.com>
2021-06-03 14:45:44 +0100

    merged todo service to the main branch

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,8 +1,15 @@
 
-
 module.exports = class FormRendering{
     static async newTodo(req, res) {
-            res.render('createTodo')
+        res.render('createTodo')
 
+    }
+
+    static async userLogin(req, res) {
+        res.render('login')
+    }
+
+    static async registerUser(req, res) {
+        res.render('register')
     }
 }
